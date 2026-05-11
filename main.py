962780598b7a02454066adc13e0f32285ff115cb
Ajisth <reenayasho6393@gmail.com>
2026-05-11 14:38:34 +0530

    Fix event loop error by using asyncio.run()

--- main.py
+++ main.py
@@ -56,10 +56,4 @@ if __name__ == "__main__":
     import nest_asyncio
     nest_asyncio.apply()
     
-    try:
-        loop = asyncio.get_event_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        
-    loop.run_until_complete(main())
+    asyncio.run(main())
