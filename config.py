df0e66cfe7fb5dc9776d2d6813fe33ec84f6e134
Ajisth <reenayasho6393@gmail.com>
2026-05-11 14:16:54 +0530

    Update image assets in config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ LOGGER_ID = int(os.environ.get("LOGGER_ID", 0))
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
 
-IMG_START = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
-IMG_HELP = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
-IMG_PING = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
-IMG_RELOAD = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
-IMG_DEFAULT = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
+IMG_START = "https://tse4.mm.bing.net/th/id/OIP.dJlsP_XjmFw5POYAZvrKhQHaFj?r=0&w=1024&h=768&rs=1&pid=ImgDetMain&o=7&rm=3"
+IMG_HELP = "https://wallpapercave.com/wp/XpfEz41.jpg"
+IMG_PING = "https://tse4.mm.bing.net/th/id/OIP.aXkVny3mSVRGKDvni4SYEwHaE8?r=0&w=1920&h=1280&rs=1&pid=ImgDetMain&o=7&rm=3"
+IMG_RELOAD = "https://motionbgs.com/media/1292/pain-nagato.jpg"
+IMG_DEFAULT = "https://motionbgs.com/media/1292/pain-nagato.jpg"
 
 JIOSAAVN_API_PRIMARY = "https://jiosaavn-api-two-beta.vercel.app/api/search/songs?query="
 JIOSAAVN_API_SECONDARY = "https://jiosavan.ajisth007.workers.dev/api/search/songs?query="
